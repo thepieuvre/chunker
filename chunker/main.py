d148da0deb923fef05f61ee110ba0a4bdf99ac29
Alexandre di Costanzo <alex@thepieuvre.com>
2013-11-23 14:50:42 +0100

    CLI Options: Redis host and port
    [closes #2]

--- chunker/main.py
+++ chunker/main.py
@@ -17,10 +17,43 @@ def parse_cmdline():
 	parser = optparse.OptionParser(usage, version='The Pieuvre Chunker ' + VERSION) 
 	parser.add_option('--verbose', action='store_true', dest='verbose',
 		help='print verbose information')
-	parser.add_option('--redis-mode', action='store_true', dest='redis_mode',
-		help='consuming the chunker queue from the local Redis')
+	parser.add_option('--redis-url', type='string', dest='redis_url',
+		help='consuming the feeder queue from the local Redis')
+	parser.add_option('--redis-host', type='string', dest='redis_host',
+		help='consuming the feeder queue from the local Redis (host)')
+	parser.add_option('--redis-port', type='int', dest='redis_port',
+		help='consuming the feeder queue from the local Redis (port)')
 
 	options, args = parser.parse_args()
+
+	# convert redis url in host/port
+	if options.redis_url:
+		if options.redis_host or options.redis_port:
+			parser.error('Redis url incompatible with redis host/port')
+		netloc = urlparse(options.redis_url).netloc
+		if not netloc:
+			parser.error('Redis url is malformed: host/port='+netloc)
+		redis_host_port = netloc.split(':')
+		if len (redis_host_port) != 2:
+			for z in redis_host_port:
+				print "arg=" + z
+			for n in urlparse(options.redis_url):
+				print 'part ' + n 
+			print 'base=' + options.redis_url
+			parser.error('Expecting a url for redis, like redis://host.domain.fr:456/')
+		options.redis_host = redis_host_port[0]
+		options.redis_port = redis_host_port[1]
+
+	if options.redis_host:
+		if len(args) > 0: 
+			parser.error('No RSS feeds needed when REDIS in use')
+		if not options.redis_port:
+			parser.error('Please specify Redis port: --redis-port=456')
+	else:
+		if options.redis_port:
+			parser.error('Please specify Redis host: --redis-host=server.domain.fr')
+		if len(args) == 0:
+			parser.error('No RSS feeds given')
 
 	return options, args
 
@@ -28,7 +61,7 @@ def main():
 	"""Starting the Pieuvre feeder"""
 	locale.setlocale(locale.LC_ALL, '')
 	options, args = parse_cmdline()
-	if options.redis_mode:
-		r = redis.StrictRedis(host='localhost', port=6379, db=0)
+	if options.redis_host:
+		r = redis.StrictRedis(host=options.redis_host, port=options.redis_port, db=0)
 		r.sadd('queues', 'chunker')
 		redis_mode(r)
